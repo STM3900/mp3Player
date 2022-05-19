5434ed2812b460899e15cd52f99952e13e5bf249
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-05-19 12:21:24 +0200

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,25 @@
 SoftwareSerial mySoftwareSerial(2, 8); // RX, TX ( wemos D2,D8 ou 4,15 GPIO )  ou Tx,RX ( Dfplayer )
 DFRobotDFPlayerMini myDFPlayer;        // init player
 
+const int buttonPreviousPin = 7;
+const int buttonPausePin = 6;
+const int buttonNextPin = 5;
+
+int buttonPreviousState = 0;
+int buttonPauseState = 0;
+int buttonNextState = 0;
+
+bool isPressed = false;
+bool isPaused = false;
+
 void setup()
 {
   mySoftwareSerial.begin(9600);
   Serial.begin(9600); // optionnel pour info dans terminal
+
+  pinMode(buttonPreviousPin, INPUT);
+  pinMode(buttonPausePin, INPUT);
+  pinMode(buttonNextPin, INPUT);
 
   if (!myDFPlayer.begin(mySoftwareSerial))
   { // Utilisation de  softwareSerial pour communiquer
@@ -41,4 +56,46 @@ void setup()
 
 void loop()
 {
+  buttonPreviousState = digitalRead(buttonPreviousPin);
+  buttonPauseState = digitalRead(buttonPausePin);
+  buttonNextState = digitalRead(buttonNextPin);
+
+  if (buttonPreviousState == HIGH && !isPressed)
+  {
+    Serial.println(F("Bouton previous allumé"));
+    myDFPlayer.previous();
+  }
+
+  if (buttonPauseState == HIGH && !isPressed)
+  {
+    if (!isPaused)
+    {
+      Serial.println(F("Bouton pause allumé"));
+      myDFPlayer.pause();
+      isPaused = true;
+    }
+    else
+    {
+      Serial.println(F("Bouton play allumé"));
+      myDFPlayer.start();
+      isPaused = false;
+    }
+  }
+
+  if (buttonNextState == HIGH && !isPressed)
+  {
+    Serial.println(F("Bouton next allumé"));
+    myDFPlayer.next();
+  }
+
+  if (buttonPreviousState == HIGH || buttonPauseState == HIGH || buttonNextState == HIGH)
+  {
+    isPressed = true;
+  }
+  else
+  {
+    isPressed = false;
+  }
+
+  //  myDFPlayer.volume(30);
 }
